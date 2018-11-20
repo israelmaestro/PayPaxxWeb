3e3abf12bbb78995e2049cdfc103faa9818fbe6f
Israel Oliveira Santos <israel.santos@visionnaire.local>
2018-11-20 11:07:35 -0200

    Initial commit

---
+++ app/src/main/java/com/hssolution/paodiariolite/MainActivity.java
@@ -0,0 +1,39 @@
+package com.hssolution.paodiariolite;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String url = "https://paodiario.org/hoje/";
+        WebView view = (WebView)this.findViewById(R.id.simpleWebView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
+        view.setWebViewClient(new MyBrowser());
+        view.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            view.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+    }
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading (WebView view,String url){
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
