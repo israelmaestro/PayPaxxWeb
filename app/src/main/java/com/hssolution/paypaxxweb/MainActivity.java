8c658607c53504d588c8d5af162bd9850a17bd7f
israelmaestro <israelmaestro@gmail.com>
2018-11-27 18:24:04 -0200

    Trocando ícones, renomeando aplicativo

--- app/src/main/java/com/hssolution/paypaxxweb/MainActivity.java
+++ app/src/main/java/com/hssolution/paypaxxweb/MainActivity.java
@@ -18,26 +18,27 @@ import com.hssolution.paypaxxweb.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("SetJavaScriptEnabled")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        String url = "https://meuscartoes.paypaxx.com.br";
-        WebView view = this.findViewById(R.id.simpleWebView);
-        view.getSettings().setJavaScriptEnabled(true);
-        view.loadUrl(url);
-        view.setWebViewClient(new MyBrowser());
-        view.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            view.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        }
-    }
-    private class MyBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading (WebView view,String url){
-            view.loadUrl(url);
-            return true;
-        }
-    }
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		String url = "https://meuscartoes.paypaxx.com.br";
+		WebView view = this.findViewById(R.id.simpleWebView);
+		view.getSettings().setJavaScriptEnabled(true);
+		view.loadUrl(url);
+		view.setWebViewClient(new MyBrowser());
+		view.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			view.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+		}
+	}
+
+	private class MyBrowser extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+	}
 }
